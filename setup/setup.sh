31a8d4e3169a6fffae8bf63eae8f81e323a9014f
Paul-weqe <paul1tw1@gmail.com>
2025-04-22 12:10:56 +0300

    test: proposal of autogeneration of
    Proposal for how to auto generate the events & config files to be
    used in the conformance tests.
    
    When running conformance tests, we need the following:
    - config.json
    - events.json
    
    Both of which can be automatically generated. This commit makes that
    a possibility.
    
    Since this commit is only a proposal, this has only been implemented for
    the VRRP topologies
    
    Inside our setup directory, we have an empty env.txt file, we should
    populate this file with the path to our holo project directory...like
    so:
    
    ```
    SNAPSHOT_DIR=/my/holo/directory
    ```
    
    When we run the munet topology using `munet -c
    vrrp/vrrpv3-topo-1-1/munet.yaml`, there will automatically be
    config.json and events.json files for each of the devices in the topo
    that are generated.
    
    Work in progress to get the output directory files(ibus.jsonl,
    protocol.jsonl, etc...) also included.
    
    Signed-off-by: Paul Wekesa <paul1tw1@gmail.com>

---
+++ setup/setup.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+protocolname=$1
+topologyname=$2
+sleeptime=$3
+eventsfile=$4
+
+source /tmp/munet/${MUNET_NODENAME}/holo.setup/env.txt
+chown holo:holo -R /var/run/holo
+holod &
+while ! holo-cli --file /etc/holo/holo.config; do sleep 1; done
+
+collect_network_snapshot() {
+  local snapshot_dir=$1
+  local sleep_duration=$2
+  local events_file=$3
+  local toponame=$4
+
+  nodename=${MUNET_NODENAME}
+
+  # Wait for initial network convergence.
+  sleep "${sleep_duration}"
+
+  # Ensure snapshot directory exists.
+  mkdir -p "${snapshot_dir}/${toponame}/${nodename}"
+
+  # Collect running configuration in JSON format.
+  holo-cli -c "show running format json" > "${snapshot_dir}/${toponame}/${nodename}/config.json"
+
+  # Collect protocol events.
+  cp "/tmp/munet/${nodename}/var.run.holo/${events_file}" "${snapshot_dir}/${toponame}/${nodename}/events.jsonl"
+  sed -i '/"Northbound":/d' "${snapshot_dir}/${toponame}/${nodename}/events.jsonl"
+}
+
+if [[ -n "${SNAPSHOT_DIR}" ]]; then
+  export SNAPSHOT_DIR=${SNAPSHOT_DIR}/${protocolname}/tests/conformance/topologies/
+  collect_network_snapshot "${SNAPSHOT_DIR}" "${sleeptime}" "${eventsfile}" "${topologyname}"
+fi
